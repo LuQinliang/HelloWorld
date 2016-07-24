bfeeae395e46b11e3a4d0f67525f4ff4a7cf6d48
LuQinliang <yunmenglingxiao@gmail.com>
2016-07-24 15:36:13 +0800

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Console.ReadKey();
+        }
+    }
+}
